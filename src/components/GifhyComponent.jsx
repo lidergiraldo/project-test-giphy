2e9825e9bb8ab84f8ca91674c00b74f10c2f5394
Liderman Giraldo <lidermangiraldo@hotmail.com>
2023-06-06 21:10:56 -0500

    Test de Async/Await - Giphy

--- src/components/GifhyComponent.jsx
+++ src/components/GifhyComponent.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 export const GifhyComponent = () => {
 
-    const apiKey = 'WoB0pgo11V0na4oFMEiY8Vcp6EdKX4K3';
-    const httpCode = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+    const [imageUrl, setImageUrl] = useState('');
 
-    httpCode
-        .then(response => response.json())
-        .then( ({ data }) => {
-            const { url } = data.images.original;
-            const img  = document.createElement('img');
-            img.src = url;
+    useEffect(() => {
+        const obtenerImagen = async () => {
+            try {
+                const apiKey = 'WoB0pgo11V0na4oFMEiY8Vcp6EdKX4K3';
+                const response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+                const { data } = await response.json();
 
-            document.body.append( img );
+                const { url } = data.images.original;
+                setImageUrl(url);
+            } catch (error) {
+                console.log(error);
+            }
+        };
 
-        })
-        .catch(error => {
-            console.log(error);
-        })
-}
+        obtenerImagen();
+    }, []);
 
+    return (
+        <div className='container-giphy'>
+            <img className="image-giphy" src={imageUrl} alt="Gif" />
+        </div>
+    );
+};
